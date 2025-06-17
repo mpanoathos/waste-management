389fef9e9c6e9e9af2265098046ed6bc95fae5b4
Mpano Athos <athosmpano427@gmail.com>
2025-06-18 01:32:07 +0200

    Frontend

--- waste-mgt-bn/prisma/migrations/20250617120149_add_approval_status/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "ApprovalStatus" AS ENUM ('PENDING', 'APPROVED', 'REJECTED');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "approvalStatus" "ApprovalStatus" NOT NULL DEFAULT 'PENDING';
